b2f4c3970eb8902f07eced2bade1da7224cb9067
chetsadakonoat <Chetsadakon.333@gmail.com>
2024-12-24 17:07:57 +0700

    google-login-v0.0.1

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,22 @@
+import { SocialAuthService } from '@abacritt/angularx-social-login';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: false,
-  
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  user : any
+  loggedIn :any
+  constructor(private authService: SocialAuthService) { }
 
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      console.log(this.user)
+    });
+  }
 }

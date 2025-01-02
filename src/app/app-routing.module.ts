7fc6f1e26b2c8ea2c80630eae9dafb7767529ae9
chetsadakonoat <Chetsadakon.333@gmail.com>
2025-01-02 10:15:04 +0700

    add-path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminUploadComponent } from './admin-upload/admin-upload.component';
+import { AdminFileINFOComponent } from './admin-file-info/admin-file-info.component';
+import { AdminDataComponent } from './admin-data/admin-data.component';
 
 const routes: Routes = [
-  {path:"" , component:LoginComponent}
+  {path:"" , component:LoginComponent},
+  {path: "admin-home", component: AdminHomeComponent},
+  {path: "admin-upload", component: AdminUploadComponent},
+  {path: "admin-info", component: AdminFileINFOComponent},
+  {path: "admin-data", component: AdminDataComponent},
+
 ];
 
 @NgModule({
